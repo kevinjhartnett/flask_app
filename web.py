50a8c07ec67c57e825cad0b61a8786e7a7a87bec
Kevin J Hartnett <kevinjhartnett@KJH.local>
2017-02-02 08:42:26 +0800

    Getting project ready for Heroku

--- web.py
+++ web.py
@@ -15,6 +15,7 @@ def about():
 	return render_template('about.html')
 
 if __name__ == "__main__":
-	app.run()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port)
 
 #run a web server on your own computer using flask
